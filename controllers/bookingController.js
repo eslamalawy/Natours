b48b216d48a81d6f5012bc0a837d15ebbf924534
eslam alawy <eslammo4kala@gmail.com>
2023-06-16 13:00:55 +0300

    some fixs

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -38,10 +38,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 
   //3) create session as response
-  // res.status(200).json({
-  //   status: "success",
-  //   session,
-  // }); // to avoid cors
+  res.status(200).json({
+    status: "success",
+    session,
+  });
 });
 
 // exports.createBookingCheckout = catchAsync(async (req, res, next) => {
